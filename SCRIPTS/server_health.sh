054b8c9476b7e473a2ff253e17ab7d2d6baddb6f
Nikhil Prasad <nikhilprasad78630@gmail.com>
2023-02-28 18:40:01 +0000

    updated

--- SCRIPTS/server_health.sh
+++ SCRIPTS/server_health.sh
@@ -1,68 +1,47 @@
-#! /bin/bash
-# clear the screen
-#clear
-# Check OS Release Version and Name
-echo -e "\033[32m --------------- OS Version -------------------------------------------------------\033[0m"
-osversion=` cat /etc/redhat-release`
-echo -e '\E[32m'"OS Release     :" $osversion
-echo -e "\033[32m --------------------- Kernel Release ------------------------------------------------\033[0m"
-# Check Kernel Release
-kernelrelease=$(uname -r)
-echo -e '\E[32m'"Kernel Release :" $kernelrelease
-echo -e "\033[32m -----------------------------------------------------------------------------------\033[0m"
-echo -e "\033[32m ----------------- Hostname of the Server ------------------------------------------\033[0m"
-# Check hostname
-echo -e '\E[32m'"Hostname       :" $HOSTNAME
-echo -e "\033[32m -----------------------------------------------------------------------------------\033[0m"
-# Check Internal IP
-internalip=$(hostname -I)
-echo -e '\E[32m'"Internal IP    :" $internalip
-echo -e "\033[32m ------------------Memory Usage------------------------------------------------------\033[0m"
-mem_TOTAL=`/usr/bin/free -m |awk '{print $2}' |head -2 | tail -1`
-mem_AVAIL=`/usr/bin/free -m |awk '{print $7}' |head -2 | tail -1`
-real_MEMUSED=`expr $mem_TOTAL - $mem_AVAIL`
-real_MEMPRCNT=`expr $real_MEMUSED \* 100`
-real_TOTMEMUSED=`echo "scale=2;  $real_MEMPRCNT / $mem_TOTAL" | bc -l`
-MEM=`echo $real_TOTMEMUSED | awk -F. '{ print $1 }'`
-echo -e '\E[32m'"Memory Usage is    :" $MEM
-if [ $MEM -gt 90 ]
-then
-echo -e '\E[31m'"Memory Usage is Critical"
-else
-echo -e '\E[32m'"Memory Usage is OK"
-fi
-echo -e "\033[32m -------------------Disk Usage -----------------------------------------------------\033[0m"
-dusage=$(df -Ph | grep -vE '^tmpfs|cdrom' | sed s/%//g | awk '{ if($5 > 60) print $0;}')
-fscount=$(echo "$dusage" | wc -l)
-if [ $fscount -ge 2 ]; then
-echo -e '\E[32m'"$dusage"
-echo -e '\E[31m'"Above partition breached threshold"
-else
-echo "Disk usage is in under threshold"
-fi
-echo -e "\033[32m -------------------Load Average-----------------------------------------------------\033[0m"
-# Check Load Average
-loadaverage=$(top -n 1 -b | grep "load average:" | awk '{print $10 $11 $12}')
-echo -e '\E[32m'"Load Average :" $tecreset $loadaverage
-echo -e "\033[32m -------------------Uptime-----------------------------------------------------------\033[0m"
-# Check System Uptime
-tecuptime=$(uptime | awk '{print $3,$4}' | cut -f1 -d,)
-echo -e '\E[32m'"System Uptime Days/(HH:MM) :" $tecreset $tecuptime
-echo -e "\033[32m ----------------CPU Utilizatio ------------------------------------------------------\033[0m"
-CPU=`top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}'`
-echo -e '\E[32m'"CPU Utilization :" $CPU
+#!/bin/bash
 
-# Unset Variables
-unset tecreset os architecture kernelrelease internalip externalip nameserver loadaverage
-#--------Print top 5 Memory & CPU consumed process threads---------#
-echo -e "\033[32m -------------------Top 5 Memory Process ----------------------------------------------\033[0m"
-echo -e "\n\nTop 5 Memory Resource Hog Processes"
-echo -e "$D$D"
-echo -e "\033[32m -------------------Top 5 CPU Process -------------------------------------------------\033[0m"
-ps -eo pmem,pid,ppid,user,stat --sort=-pmem|grep -v $$|head -6|sed 's/$/\n/'
-echo -e "\nTop 5 CPU Resource Hog Processes"
-echo -e "$D$D"
-ps -eo pcpu,pid,ppid,user,stat,args --sort=-pcpu|grep -v $$|head -6|sed 's/$/\n/' | awk '{print $1"\t" $2"\t" $4"\t" $6}'
-#CPU=`top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}'`
-#echo -e '\E[32m'"CPU Utilization :" $CPU
+# Get OS version and kernel release
+os_version=$(cat /etc/*-release | grep "PRETTY_NAME" | cut -d "=" -f 2-)
+kernel_release=$(uname -r)
+
+# Get hostname and internal IP address
+hostname=$(hostname)
+internal_ip=$(hostname -I | awk '{print $1}')
+
+# Get memory usage percentage
+mem_total=$(free -m | awk '/^Mem:/{print $2}')
+mem_used=$(free -m | awk '/^Mem:/{print $3}')
+mem_free=$(free -m | awk '/^Mem:/{print $4}')
+mem_usage=$(echo "scale=2; $mem_used/$mem_total * 100" | bc)
+
+# Get disk usage percentage
+disk_usage=$(df -h / | awk '/\//{print $(NF-1)}')
+
+# Get load average
+load_average=$(uptime | awk -F'average: ' '{print $2}')
+
+# Get system uptime
+uptime=$(uptime | awk '{print $3,$4}' | sed 's/,//')
+
+# Get CPU utilization percentage
+cpu_usage=$(mpstat 1 1 | awk '$12 ~ /[0-9.]+/ { print 100 - $12"%" }')
+
+# Get top 5 memory usage processes
+mem_processes=$(ps aux --sort=-%mem | awk 'NR<=6{print $0}')
+
+# Get top 5 CPU usage processes
+cpu_processes=$(ps aux --sort=-%cpu | awk 'NR<=6{print $0}')
+
+# Print system information with color coding
+echo -e "\033[32mOS Version     : $os_version\033[0m"
+echo -e "\033[32mKernel Release : $kernel_release\033[0m"
+echo -e "\033[32mHostname       : $hostname\033[0m"
+echo -e "\033[32mInternal IP    : $internal_ip\033[0m"
+echo -e "\033[32mMemory Usage   : $mem_usage%\033[0m"
+echo -e "\033[32mDisk Usage     : $disk_usage\033[0m"
+echo -e "\033[32mLoad Average   : $load_average\033[0m"
+echo -e "\033[32mSystem Uptime  : $uptime\033[0m"
+echo -e "\033[32mCPU Utilization: $cpu_usage\033[0m"
+echo -e "\033[32mTop 5 Memory Resource Hog Processes:\033[0m\n$mem_processes"
+echo -e "\033[32mTop 5 CPU Resource Hog Processes:\033[0m\n$cpu_processes"
 
